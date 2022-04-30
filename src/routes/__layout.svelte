687eea5140bb9f7b74a44e9d05607d360c7a4425
Troy Ciesco <troy.ciesco@hansoninc.com>
2022-04-30 19:17:39 -0400

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+	<slot />
+
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/interactive-rating-component">interactive-rating-component</a></li>
+		<li><a href="/qr-code-component">qr-code-component</a></li>
+	</ul>
+
+<style>
+ul {
+	position: absolute;
+	bottom: 16px;
+	left: 16px;
+}
+</style>
