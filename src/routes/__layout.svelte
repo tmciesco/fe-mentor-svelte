96c49f299182640770d5e9c81da6f6fde1173b6c
Troy Ciesco <troy.ciesco@hansoninc.com>
2022-04-30 21:12:27 -0400

    add burger menu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,92 @@
 <script lang="ts">
   import Header from '$lib/header/Header.svelte';
   import '../app.css';
+	let isDrawerOpen: boolean = false;
+	import { fade, fly } from 'svelte/transition';
+
+	let duration = 200
+
+	function toggleDrawer() {
+		setTimeout(() => {
+			window.document.body.classList.toggle('open-drawer')
+		}, isDrawerOpen ? duration + 100 : 0);
+			isDrawerOpen = !isDrawerOpen
+	}
 </script>
 
+<button on:click={toggleDrawer}>
+	<i class="ph-hamburger"></i>
+</button>
 <slot />
 
-<ul>
-  <li><a href="/">Home</a></li>
-  <li><a href="/interactive-rating">interactive-rating</a></li>
-  <li><a href="/qr-code">qr-code</a></li>
-  <li><a href="/stats-preview-card">stats-preview-card</a></li>
-</ul>
+{#if isDrawerOpen}
+<div class="drawer" in:fly={{x: 100, duration}} out:fly={{x: 100, duration}}>
+	<button on:click={toggleDrawer}>
+		<i class="ph-x"></i>
+	</button>
+	<ul>
+		<li><a on:click={toggleDrawer} href="/">Home</a></li>
+		<li><a on:click={toggleDrawer} href="https://github.com/tmciesco/fe-mentor-svelte">Github</a></li>
+		<li><a on:click={toggleDrawer} href="/interactive-rating">interactive-rating</a></li>
+		<li><a on:click={toggleDrawer} href="/qr-code">qr-code</a></li>
+		<li><a on:click={toggleDrawer} href="/stats-preview-card">stats-preview-card</a></li>
+	</ul>
+</div>
+<div class="overlay" in:fade={{duration}} out:fade={{duration}}></div>
+{/if}
 
 <style>
-  ul {
-    position: absolute;
-    top: 16px;
-    left: 16px;
+	:global(body.open-drawer) {
+		overflow: hidden;
+	}
+  button {
+		position: absolute;
+		top: 16px;
+		right: 16px;
+		background-color: #97d1d5;
+		border: none;
+		cursor: pointer;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		padding: 8px;
+	}
+	i {
+		color: #141883;
+		font-size: 32px;
   }
+
+	.drawer {
+		position: absolute;
+		top: 0;
+		right: 0;
+		z-index: 10;
+		min-height: 100vh;
+		background-color: #ffffff;
+		padding: 80px 112px 0 36px;
+		overflow: hidden;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	li {
+		margin-bottom: 16px;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	.overlay {
+		position: absolute;
+		top:0;
+		left:0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(189, 7, 7, 0.507);
+		z-index: 2;
+	}
+
 </style>
