de5eade7a14014b523dd05cf8c9d8af6ef8e2af9
Troy Ciesco <troy.ciesco@hansoninc.com>
2022-04-30 20:28:33 -0400

    prettier

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+  import Header from '$lib/header/Header.svelte';
+  import '../app.css';
 </script>
 
-	<slot />
+<slot />
 
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="/interactive-rating">interactive-rating</a></li>
-		<li><a href="/qr-code">qr-code</a></li>
-		<li><a href="/stats-preview-card">stats-preview-card</a></li>
-	</ul>
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/interactive-rating">interactive-rating</a></li>
+  <li><a href="/qr-code">qr-code</a></li>
+  <li><a href="/stats-preview-card">stats-preview-card</a></li>
+</ul>
 
 <style>
-ul {
-	position: absolute;
-	top: 16px;
-	left: 16px;
-}
+  ul {
+    position: absolute;
+    top: 16px;
+    left: 16px;
+  }
 </style>
--- src/routes/qr-code.svelte
+++ src/routes/qr-code.svelte
@@ -1,89 +1,93 @@
 <script lang="ts">
-	import { onMount } from "svelte"
-	import { fly } from "svelte/transition"
+  import { onMount } from 'svelte';
+  import { fly } from 'svelte/transition';
 
-	let isLoaded: boolean = false
+  let isLoaded: boolean = false;
 
-	onMount(() => {
-		isLoaded = true
-	})
+  onMount(() => {
+    isLoaded = true;
+  });
 </script>
 
 <main>
-	{#if isLoaded}
-		<div class="card" in:fly={{ y: 100 }}>
-			<div class="img-container">
-				<img src="images/image-qr-code.png" alt="Frontend Mentor QR Code" />
-			</div>
-			<h1 class="title">Improve your frontend skills by building projects</h1>
-			<p class="description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level!</p>
-		</div>
-	{/if}
-	<div class="attribution">
-		Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by
-		<a href="https://troycies.co">Troy Ciesco</a>.
-	</div>
+  {#if isLoaded}
+    <div class="card" in:fly={{ y: 100 }}>
+      <div class="img-container">
+        <img src="images/image-qr-code.png" alt="Frontend Mentor QR Code" />
+      </div>
+      <h1 class="title">Improve your frontend skills by building projects</h1>
+      <p class="description">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level!
+      </p>
+    </div>
+  {/if}
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by
+    <a href="https://troycies.co">Troy Ciesco</a>.
+  </div>
 </main>
 
 <style>
-	p {
-		margin: 0;
-		padding: 0;
-	}
-	main {
+  p {
+    margin: 0;
+    padding: 0;
+  }
+  main {
     background-color: #d5e1ef;
-		min-height: 100vh;
-		text-align: center;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex-grow: 1;
-	}
-	.card {
-		width: 320px;
-		background: #ffffff;
-		margin-bottom: 3rem;
-		padding: 16px;
-		border-radius: 20px;
+    min-height: 100vh;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+  }
+  .card {
+    width: 320px;
+    background: #ffffff;
+    margin-bottom: 3rem;
+    padding: 16px;
+    border-radius: 20px;
     box-shadow: 0px 25px 25px rgba(0, 0, 0, 0.0476518);
-	}
+  }
 
-	.img-container {
-		width: 100%;
-		height: 288px;
-		border-radius: 10px;
-		margin-bottom: 24px;
-	}
+  .img-container {
+    width: 100%;
+    height: 288px;
+    border-radius: 10px;
+    margin-bottom: 24px;
+  }
 
-	img {
-		width: inherit;
-		height: inherit;
-		border-radius: 10px;
-	}
+  img {
+    width: inherit;
+    height: inherit;
+    border-radius: 10px;
+  }
 
-	.title {
-		text-align: center;
-		font-size: 22px;
-		font-weight: 700;
-		margin-bottom: 8px;
-		color: #1f314f;
-	}
+  .title {
+    text-align: center;
+    font-size: 22px;
+    font-weight: 700;
+    margin-bottom: 8px;
+    color: #1f314f;
+  }
 
-	.description {
-		text-align: center;
-		font-size: 15px;
-		line-height: 1.5;
-		margin-bottom: 24px;
-		color: #7d889e;
-	}
+  .description {
+    text-align: center;
+    font-size: 15px;
+    line-height: 1.5;
+    margin-bottom: 24px;
+    color: #7d889e;
+  }
 
-	.attribution {
-		color: #1f314f;
-		font-size: 11px;
-		text-align: center;
-	}
-	.attribution a {
-		color: #fc7612;
-	}
+  .attribution {
+    color: #1f314f;
+    font-size: 11px;
+    text-align: center;
+  }
+  .attribution a {
+    color: #fc7612;
+  }
 </style>
